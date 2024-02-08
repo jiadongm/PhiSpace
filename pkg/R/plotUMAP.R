53c667c4d253d62a63d29c47771cfb1d9fc2587d
jiadongm <chiatungmao@gmail.com>
2024-02-08 20:23:49 +1100

    computUMAP

---
+++ pkg/R/plotUMAP.R
@@ -0,0 +1,38 @@
+#' Compute UMAP.
+#'
+#' @param dat Input data.
+#' @param computPC Whether to compute PC first.
+#' @param ncomp Number of PC components
+#' @param center Center or not.
+#' @param scale Scale or not.
+#' @param labs Labels.
+#'
+#' @return UMAP coordinates.
+#' @export
+computUMAP <- function(
+    dat,
+    computPC = TRUE,
+    ncomp = 30,
+    center = TRUE,
+    scale = FALSE,
+    labs =
+){
+
+  if(computPC){
+
+    plot_dat <- getPC(
+      dat,
+      ncomp = ncomp,
+      center = center,
+      scale = scale
+    )$scores
+  } else {
+
+    plot_dat <- dat
+  }
+
+  umap::umap(
+    query_pc$scores
+  )$layout %>%
+    reNameCols()
+}
