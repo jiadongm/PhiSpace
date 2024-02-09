4be31ef325561c3f61b92ca3219b98a7b6a851d4
jiadongm <chiatungmao@gmail.com>
2024-02-09 20:25:44 +1100

    PB function faster

--- pkg/R/pseudoBulk.R
+++ pkg/R/pseudoBulk.R
@@ -1,114 +1,109 @@
 #' Converting single cells to pseudo-bulks.
 #'
-#' @param query `SingleCellExpeirment` object.
-#' @param clusterid Character.
-#' @param assay Character.
-#' @param size.factor Numeric.
-#' @param pool.factor Numeric.
-#' @param nPool Integer.
-#' @param YY Matrix.
+#' @param sce `SingleCellExpeirment` object.
+#' @param clusterid Column name of colData for specifying groups for pseudobulking.
+#' @param phenotypes Phenotypes to predict. Can be multiple.
+#' @param response Response matrix. Can be continuous.
+#' @param assayName Assay used for pseudobulking.
+#' @param resampSizes How many pseudo-bulk resamples to generate from each cluster.
+#' @param proportion Used to determine unequal resampSizes.
+#' @param seed Random seeds
+#' @param nPool How many cells to use for pseudobulking.
 #'
-#' @return An updated `SingleCellExpeirment` object with a pseudobulk assay
+#' @return An updated `SingleCellExpeirment` object with a pseudobulk assay.
 #'
 #' @export
-pseudoBulk <- function(query,
-                       clusterid,
-                       assay = 'counts',
-                       size.factor = 1,
-                       pool.factor = 1,
-                       nPool = 15,
-                       YY = NULL)
-{
-  cluster <- colData(query)[, clusterid]
-  cluname <- sort(unique(cluster))
-  Ncluster <- length(cluname)
-  G <- nrow(query)
+pseudoBulk <- function(
+    sce,
+    phenotypes = NULL,
+    response = NULL,
+    clusterid = NULL,
+    assayName = 'counts',
+    resampSizes = 100,
+    proportion = NULL,
+    nPool = 15,
+    seed = 904800
+){
 
-  if(length(size.factor)==1){
-    sizeFacVec <- rep(size.factor, Ncluster)
-  } else if (length(size.factor)==Ncluster) {
-    sizeFacVec <- size.factor[cluname]
+  ## Response matrix
+  if(is.null(response)){
+
+    if(is.null(phenotypes)) stop("Phenotypes and response cannot both be NULL.")
+
+    YY <- codeY(sce, phenotypes)
+
+    if(is.null(clusterid)) clusterid <- phenotypes[1]
   } else {
-    stop("Length of size.factor has to be either 1 or same as number of clusters/celltypes.")
+
+    YY <- response
   }
 
-  outList <- outYYlist <- vector("list", length(cluname))
-  outCluster <- c()
+  ## Resampling indices
+  nGenes <- nrow(sce)
+  nCells <- ncol(sce)
+  # Index list
+  cluster <- as.character(colData(sce)[, clusterid])
+  cluname <- sort(unique(cluster))
+  idxList <- split(1:nCells, cluster)
+  names(idxList) <- cluname
+  Ncluster <- length(cluname)
+  clustSizes <- sapply(idxList, length)
+  # How many to resample
+  if(is.null(proportion)){
 
-  if(!is.null(YY)){
-    if(nrow(YY)!=ncol(query)) stop("nrow of YY has to be equal to #cells in query.")
+    if(length(resampSizes) == 1){
+
+      resampSizes <- rep(resampSizes, Ncluster)
+    } else {
+
+      if(length(resampSizes) != Ncluster) stop("Length of resampSizes has to be either 1 or number of clusters.")
+    }
+
+  } else {
+
+    resampSizes <- ceiling(clustSizes * proportion)
   }
+  # Resample
+  set.seed(seed)
+  resampIdx <- lapply(
+    1:Ncluster,
+    function(x){
 
-  #Aggregate cells
-  for(i in cluname){
-    message(paste("\r Now aggregate", i), appendLF = F)
-    subQuery <- Matrix::Matrix(assay(query, assay)[, cluster == i], sparse = T)
-    if(!is.null(YY)){
-      subYY <- YY[cluster == i, , drop=F]
+      resampSize <- resampSizes[x]
+      out <- split(
+        sample(idxList[[x]], nPool*resampSize, replace = TRUE),
+        rep(1:resampSize, rep(nPool, resampSize))
+      )
+      names(out) <- NULL
+      return(out)
     }
-    nCell <- ncol(subQuery)
-    nOut <- ceiling(nCell * sizeFacVec[i])
-    if(is.null(nPool)) nPool <- ceiling(nCell * pool.factor)
+  )
+  resampIdx <- do.call(c, resampIdx)
+  # Aggregate X
+  XX <- assay(sce, assayName)
+  XXagg <- sapply(
+    resampIdx,
+    function(x){
 
-    subOut <- matrix(NA, nrow = G, ncol = nOut)
-    colnames(subOut) <- paste(i, '.', 1:nOut, sep = '')
-    if(!is.null(YY)){
-      subYYout <- matrix(NA, nrow = nOut, ncol = ncol(subYY))
-      colnames(subYYout) <- colnames(YY)
+      rowMeans(XX[,x])
     }
+  )
+  # Aggregate Y
+  YYagg <- t(
+    sapply(
+      resampIdx,
+      function(x){
 
-
-
-    sampleIndex <- sample(1:nCell, nOut * nPool, replace = T) %>%
-      matrix(ncol = nPool, nrow = nOut)
-
-    if(nPool > 1){
-      for(j in 1:nOut){
-        subOut[,j] <- rowSums(subQuery[,sampleIndex[j,]])/nPool
-        if(!is.null(YY)){
-          subYYout[j,] <- colSums(subYY[sampleIndex[j,], ])/nPool
-        }
+        colMeans(YY[x,])
       }
-    }else{
-      for(j in 1:nOut){
-        subOut[,j] <- subQuery[,sampleIndex[j,]]/nPool
-        if(!is.null(YY)){
-          subYYout[j,] <- subYY[sampleIndex[j,], ]/nPool
-        }
-      }
-    }
+    )
+  )
+  colnames(XXagg) <- rownames(YYagg) <- paste0("PB", 1:ncol(XXagg))
 
-    outList[[i]] <- subOut
-    if(!is.null(YY)){
-      colnames(subYYout) <- colnames(YY)
-      outYYlist[[i]] <- subYYout
-    }
-    outCluster <- c(outCluster, rep(i, nOut))
-  }
-
-  #Collapse the list
-  outList <- unname(outList)
-  outData <- do.call(cbind, outList)
-  rownames(outData) <- rownames(query)
-
-  #remove duplicated pseudo-bulk samples.
-  # dup <- duplicated(t(outData)) | duplicated(t(outData)[ncol(outData):1,])[ncol(outData):1]
-  # outData <- outData[,!dup]
-  # outCluster <- outCluster[!dup]
-
-
-  sce <- SingleCellExperiment(list(data = outData))
-  colData(sce)[,clusterid] <- outCluster
-  if(!is.null(YY)){
-    outYYlist <- unname(outYYlist)
-    outYY <- do.call(rbind, outYYlist)
-    rownames(outYY) <- colnames(outData)
-    colData(sce) <- cbind(colData(sce), outYY)
-  }
-
-
-
-  message(paste('\n Sparsity after imputation is', round(mean(outData == 0),3)))
-  return(sce)
+  # Output
+  SingleCellExperiment(
+    list(data = XXagg),
+    colData = YYagg
+  )
 
 }
